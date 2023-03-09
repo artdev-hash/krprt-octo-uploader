1fb8df269bfa6def0864a9aa029ef0c304eb97a8
artdev-hash <kim.dev.pr@gmail.com>
2023-03-09 22:28:45 +0800

    upgrading env

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./krprt-octo-uploader', import.meta.url))
+      '@': fileURLToPath(new URL('./krprt-octo-uploader/', import.meta.url))
     }
   }
 })
